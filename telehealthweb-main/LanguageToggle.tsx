9345e8599e7fe0dff66d3590d77c4df404dd575c
amniellys0130 <amniellys@hotmail.com>
2025-07-06 14:23:53 -0400

    Add files via upload

---
+++ telehealthweb-main/LanguageToggle.tsx
@@ -0,0 +1,8 @@
+export default function LanguageToggle() {
+  return (
+    <div className="text-right p-4">
+      <a href="/" className="mr-4">🇺🇸 English</a>
+      <a href="/es">🇪🇸 Español</a>
+    </div>
+  );
+}
---
+++ telehealthweb-main/sobre-nosotros.tsx
@@ -0,0 +1,1 @@
+// Placeholder for es/sobre-nosotros.tsx
